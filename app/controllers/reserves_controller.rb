fd6c6f8e9904e269e5bb3dca14d07061f2f3a39a
iamyunusali <aliyunus291@gmail.com>
2023-01-07 14:22:10 +0300

    Add reserve contoller

---
+++ app/controllers/reserves_controller.rb
@@ -0,0 +1,39 @@
+class ReservesController < ApplicationController
+  before_action :set_reserf, only: %i[ show update destroy ]
+
+
+  def index
+    @reserves = Reserve.where(user_id: params[:user_id])
+
+    render json: @reserves
+  end
+
+
+  def show
+    render json: @reserf
+  end
+
+
+  def create
+    @reserf = Reserve.where(user_id: params[:user_id] , event_id: params[:event_id]).new(reserf_params)
+
+    if @reserf.save
+      render json: @reserf, status: :created
+    else
+      render json: @reserf.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reserf.destroy
+  end
+
+  private
+    def set_reserf
+      @reserf = Reserve.find(params[:id])
+    end
+
+    def reserf_params
+      params.require(:reserf).permit(:name, :date, :city, :user_id, :event_id)
+    end
+end
