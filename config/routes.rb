a108aa8bbc97fdd6d190ff38222a8c6148615ae8
iamyunusali <aliyunus291@gmail.com>
2023-01-07 14:21:41 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :events
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
