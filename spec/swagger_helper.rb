7da37e6508684a56e2d1b2cdc4b220cfc3483ef2
iamyunusali <aliyunus291@gmail.com>
2023-01-10 10:24:46 +0300

    add swagger helper

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,10 +24,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'https://eventifyhub.herokuapp.com/',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: 'https://eventifyhub.herokuapp.com/'
             }
           }
         }
