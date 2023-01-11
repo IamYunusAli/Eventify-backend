b0dfba383229424382ac7e4f24eaf725fbf30ad9
iamyunusali <aliyunus291@gmail.com>
2023-01-11 11:10:08 +0300

    fix linter errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'User test', type: :model do
-    subject do
-        User.new(username: 'Yunus')
-      end
+  subject do
+    User.new(username: 'Yunus')
+  end
 
-    it 'the given subject should be valid' do
-      expect(subject).to be_valid
-    end
+  it 'the given subject should be valid' do
+    expect(subject).to be_valid
+  end
 
-    it 'username is present' do
-      expect(subject.username).to be_present
-    end
+  it 'username is present' do
+    expect(subject.username).to be_present
+  end
 
-    it 'username to be Yunus' do
-      expect(subject.username).to eq('Yunus')
-    end
+  it 'username to be Yunus' do
+    expect(subject.username).to eq('Yunus')
+  end
 end
