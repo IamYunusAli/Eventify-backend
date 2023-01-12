a9d70fbd1790d9c63fb3271ede9e224700c811a7
iamyunusali <aliyunus291@gmail.com>
2023-01-12 12:02:44 +0300

    Add reserve validation

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,4 +1,7 @@
 class Reserve < ApplicationRecord
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :city, presence: true
   belongs_to :user
   belongs_to :event
 end
