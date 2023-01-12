f3f5e6b2f2bc2853df2113a1bf8a1730daa5ad41
iamyunusali <aliyunus291@gmail.com>
2023-01-12 12:01:06 +0300

    Add event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,13 @@
 class Event < ApplicationRecord
-  has_many :reserves
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :category, presence: true
+  validates :date , presence: true
+  validates :time, presence: true
+  validates :description, presence: true
+  validates :organizer, presence: true
+  validates :location, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  has_many :reserves, dependent: :destroy
   belongs_to :user
 end
