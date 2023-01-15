c43e3d719f6635f046f8b269d8c51b2284dd2ee3
iamyunusali <aliyunus291@gmail.com>
2023-01-15 11:02:05 +0300

    making username to be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { maximum: 20, minimum: 4 }
+  validates :username, presence: true, length: { maximum: 20, minimum: 4 }, uniqueness: { case_sensitive: false }
   has_many :events
   has_many :reserves
 end
