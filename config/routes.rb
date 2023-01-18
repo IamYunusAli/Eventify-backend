8c3a22fc69eb461276e32ee172343bf119a28cd8
iamyunusali <aliyunus291@gmail.com>
2023-01-18 17:39:54 +0300

    change to generate essential routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  resources :users
-  resources :events
-  resources :reserves
+  resources :users, only: [:index, :show, :create]
+  resources :events, except: [:update]
+  resources :reserves,except: [:update]
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
